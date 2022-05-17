c852b4c71cedeb794f20304054100c1f58e0b2f4
Heojaeyoung <gj30347896@gmail.com>
2022-05-17 17:56:09 +0900

    data insert

--- project/movies/admin.py
+++ project/movies/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
-from .models import Genre, Movie, Comment
+from .models import Genre, Movie
 # Register your models here.
 
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ('name', )
+    list_display = ('genre_id', 'genre_name', )
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'release_date', 'vote_average',)
+    list_display = ('title', 'released_date', 'vote_average',)
 
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('content', 'movie', 'user',)
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ('content', 'movie', 'user',)
 
 
 
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Movie, MovieAdmin)
-admin.site.register(Comment, CommentAdmin)
+# admin.site.register(Comment, CommentAdmin)
--- project/movies/models.py
+++ project/movies/models.py
@@ -3,27 +3,28 @@ from django.conf import settings
 # Create your models here.
 
 class Genre(models.Model):
-    name = models.CharField(max_length=50)
+    genre_id = models.IntegerField(unique=True)
+    genre_name = models.CharField(max_length=50)
     
     def __str__(self):
         return self.name
 
 
 class Movie (models.Model):
+    movie_id = models.IntegerField(unique=True)
     title = models.CharField(max_length=100)
-    movie_id = models.IntegerField()
-    overview = models.TextField(null=True, blank=True)
-    poster_path = models.CharField(max_length=200)
-    release_date = models.DateField()
+    released_date = models.DateField()
     vote_average = models.FloatField()
-    genre = models.ManyToManyField(Genre)
+    overview = models.TextField(null=True)
+    poster_path = models.CharField(max_length=200, null=True)
+    genres = models.ManyToManyField(Genre, related_name="movies", blank=True)
 
 
     def __str__(self):
         return self.title
 
 
-class Comment(models.Model):
-    content = models.TextField()
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+# class Comment(models.Model):
+#     content = models.TextField()
+#     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
