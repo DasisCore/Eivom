859625b474f1841a971ff1e5c845c6ffae3cc4ec
Heojaeyoung <gj30347896@gmail.com>
2022-05-18 10:00:51 +0900

    view

--- project/movies/views.py
+++ project/movies/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
+from .models import Movie
+from .serializers import MovieListSerializer
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def movie_list(request):
+    if request.method == 'GET':
+        movies = get_list_or_404(Movie)
+        serializer = MovieListSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MovieListSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def movie_detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    
+    if request.method == "GET":
+        serializer = MovieListSerializer(movie)
+        return Response(serializer.data)
+
+    elif request.method == "DELETE":
+        movie.delete()
+        data = {
+            'delete': f'데이터 {movie_pk}번이 삭제되었습니다.'
+        }
+        return Response(data, status=status.HTTP_204_NO_CONTENT)
+    
+    elif request.method == "PUT":
+        serializer = MovieListSerializer(movie, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
