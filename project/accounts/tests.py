d015ae480d206e7abffaf06e30c99fca4b89681c
hanggeee <yoonhangg@gmail.com>
2022-05-20 15:48:51 +0900

    account

--- project/accounts/tests.py
+++ project/accounts/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from movies.models import Movie
+from movies.serializers import MovieSerializer
 
-# Create your tests here.
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class MovieSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Movie
+            fields = '__all__'
+
+    like_movies = MovieSerializer(many=True)
+
+    class Meta:
+        model = User
+        fields = ('id','username','email', 'profile_img', 'followings', 'followers', 'like_movies')
+
+class UserImgSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('profile_img', 'id')
