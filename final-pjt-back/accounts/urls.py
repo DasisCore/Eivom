13580ac2aa7d33a2238bb2512371b29811c6d90e
hanggeee <yoonhangg@gmail.com>
2022-05-22 06:09:58 +0900

    url 수정

--- final-pjt-back/accounts/urls.py
+++ final-pjt-back/accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<username>/', views.user_profile),
-    path('<username>/follow/', views.follow),
-    path('<username>/upload_img/', views.upload_img),
+    path('profile/<username>/', views.user_profile),
+    path('follow/<username>/', views.follow),
+    path('upload_img/<username>/', views.upload_img),
 ]
--- final-pjt-back/finalPJT/urls.py
+++ final-pjt-back/finalPJT/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('movies/', include('movies.urls')),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
-    path('api/v1/accounts/', include('dj_rest_auth.urls')),
-    path('api/v1/accounts/signup/', include('dj_rest_auth.registration.urls')),
+    path('accounts/', include('dj_rest_auth.urls')),
+    path('accounts/signup/', include('dj_rest_auth.registration.urls')),
 ]
